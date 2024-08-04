65f743ac91f6b80fe028601ddb5d900ff3825678
premiumsmart <tvonline123@int.pl>
2024-08-04 19:09:24 +0200

    update

---
+++ tv/live/tvpsport.php
@@ -0,0 +1,7 @@
+<style>body{background:black;margin:0;padding:0;}</style>
+<script>
+if (window !== window.parent) 
+{
+document.write('<iframe src="https://embedsportek1.online/live/tvpsport.php" width="100%" height="100%" marginheight="0" marginwidth="0" scrolling="no" frameborder="0" allowfullscreen="" allow="encrypted-media"></iframe>')
+}
+</script>
